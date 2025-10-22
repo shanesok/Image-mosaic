0ac3621f7b21a48c9ea38ee6d2db4549b983c64b
shanesok <asdfa10780@gmail.com>
2025-10-22 15:07:36 +0700

    finshed code + output image

--- from PIL import Image.py
+++ from PIL import Image.py
@@ -7,9 +7,9 @@ best_img = []
 
 def main():
     n=10
-    path_input_image = "C:/Users/asdfa/OneDrive/Documents/GitHub/Image-mosaic/Pikachu.jpg"
-    path_database = "C:/Users/asdfa/OneDrive/Documents/GitHub/Image-mosaic/bart_simpson"
-    path_output = "C:/Users/asdfa/OneDrive/Documents/GitHub/Image-mosaic/output.png"
+    path_input_image = "./Pikachu.jpg"
+    path_database = "./bart_simpson"
+    path_output = "./output.png"
     I = load_image(path_input_image)
     width, height = I.size
     output = create_empty_image(width, height, n)
@@ -36,21 +36,17 @@ def create_empty_image(width, height, n):
     return ouput
 
 def compare_kernel2ranklist(rankl_list, kernel_avg):
-    counter = 0
     for i in range(len(kernel_avg)):
-        R1, G1, B1 = kernel_avg[i][1][0] - rankl_list[0][1][0], kernel_avg[i][1][1] - rankl_list[0][1][1], kernel_avg[i][1][2] - rankl_list[0][1][2]
+        R1, G1, B1 = abs(kernel_avg[i][1][0] - rankl_list[0][1][0]), abs(kernel_avg[i][1][1] - rankl_list[0][1][1]), abs(kernel_avg[i][1][2] - rankl_list[0][1][2])
         smallest_diff = R1+G1+B1
         best_img_val = rankl_list[0][0]
         for j in range(1,len(rankl_list)):
-            R2, G2, B2 = kernel_avg[i][1][0] - rankl_list[j][1][0], kernel_avg[i][1][1] - rankl_list[j][1][1], kernel_avg[i][1][2] - rankl_list[j][1][2]
+            R2, G2, B2 = abs(kernel_avg[i][1][0] - rankl_list[j][1][0]), abs(kernel_avg[i][1][1] - rankl_list[j][1][1]), abs(kernel_avg[i][1][2] - rankl_list[j][1][2])
             current_diff = R2+G2+B2
             if current_diff < smallest_diff:
-                counter += 1
                 smallest_diff = current_diff
                 best_img_val = rankl_list[j][0]
         best_img.append(best_img_val)
-    print(counter)
-    return best_img
 
 def process_kernel(I, n, width, height):
     kernel = []
@@ -84,7 +80,7 @@ def process_database(path_database):
             r = sum(p[0] for p in pixels) // len(pixels)
             g = sum(p[1] for p in pixels) // len(pixels)
             b = sum(p[2] for p in pixels) // len(pixels)
-            database.append((img_path, (r, g, b)))
+            database.append((img_path, [r, g, b]))
     return database
 
 def load_image(path_input_image):
@@ -96,8 +92,3 @@ def save_image(output, path_output):
 
 if __name__ == "__main__":
     main()
-for i in range(len(best_img)-1):
-    a = 0
-    if best_img[i] != best_img[i+1] and a < 3:
-        print(best_img[i], best_img[i+1])
-        a += 1
